7a10d2557604fc27d5d20ca763c0434e763907bd
Bryce Anglin <bryce.anglin@gmail.com>
2019-08-24 21:16:52 -0700

    added example user dataset

--- macros/js/main.js
+++ macros/js/main.js
@@ -2,20 +2,83 @@ var startTime = moment('07:00:00', 'HH:mm:ss');
 var endTime = moment('22:00:00', 'HH:mm:ss');
 var goalAmount = 200;
 
-var goalData = [];
-var currentAmount = 0;
-var totalHours = moment.duration(endTime.diff(startTime)).asHours();
-var thisTime = moment('00:00:00', 'HH:mm:ss');
-for (i = 0; i <= 24; i++) {
-  if (thisTime.isAfter(startTime) && (thisTime.isBefore(endTime) || thisTime.isSame(endTime))) {
-    currentAmount += goalAmount / totalHours;
-  }
-  goalData.push({
-    x: thisTime.format(),
-    y: currentAmount
-  });
-  thisTime.add(1, 'hour');
-}
+var goalData = [
+  {
+    x: moment('00:00:00', 'HH:mm:ss'),
+    y: 0
+  },
+  {
+    x: startTime,
+    y: 0
+  },
+  {
+    x: endTime,
+    y: goalAmount
+  },
+  {
+    x: moment('24:00:00', 'HH:mm:ss'),
+    y: goalAmount
+  },
+];
+
+var userData = [
+  {
+    x: moment('00:00:00', 'HH:mm:ss'),
+    y: 0
+  },
+  {
+    x: moment('08:05:00', 'HH:mm:ss'),
+    y: 0
+  },
+  {
+    x: moment('08:10:00', 'HH:mm:ss'),
+    y: 24
+  },
+  {
+    x: moment('08:40:00', 'HH:mm:ss'),
+    y: 24
+  },
+  {
+    x: moment('08:45:00', 'HH:mm:ss'),
+    y: 62
+  },
+  {
+    x: moment('11:47:00', 'HH:mm:ss'),
+    y: 62
+  },
+  {
+    x: moment('11:52:00', 'HH:mm:ss'),
+    y: 96
+  },
+  {
+    x: moment('13:15:00', 'HH:mm:ss'),
+    y: 96
+  },
+  {
+    x: moment('13:20:00', 'HH:mm:ss'),
+    y: 113
+  },
+  {
+    x: moment('17:52:00', 'HH:mm:ss'),
+    y: 113
+  },
+  {
+    x: moment('17:58:00', 'HH:mm:ss'),
+    y: 146
+  },
+  {
+    x: moment('19:58:00', 'HH:mm:ss'),
+    y: 146
+  },
+  {
+    x: moment('20:03:00', 'HH:mm:ss'),
+    y: 196
+  },
+  {
+    x: moment(),
+    y: 196
+  },
+];
 
 var ctx = document.getElementById('dailyProteinChart').getContext('2d');
 var myChart = new Chart(ctx, {
@@ -25,7 +88,16 @@ var myChart = new Chart(ctx, {
       data: goalData,
       lineTension: 0,
       fill: false,
-      label: "goal"
+      label: "goal",
+      pointRadius: 0,
+      pointHitRadius: 0
+    }, {
+      data: userData,
+      lineTension: 0,
+      fill: false,
+      label: "goal",
+      pointRadius: 0,
+      pointHitRadius: 0
     }]
   },
   options: {
@@ -51,5 +123,6 @@ var myChart = new Chart(ctx, {
 
 window.setInterval(function() {
   myChart.options.annotation.annotations[0].value = moment().format();
+  myChart.datasets[1].data[myChart.datasets[1].data.length].x = moment();
   myChart.update();
 }, 10000);
