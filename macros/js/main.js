c52f146c8ea39c872307767ebc8b356ed1279443
Bryce Anglin <bryce.anglin@gmail.com>
2019-08-24 20:59:40 -0700

    updated macros chart for time data and current time line

--- macros/js/main.js
+++ macros/js/main.js
@@ -1,36 +1,55 @@
+var startTime = moment('07:00:00', 'HH:mm:ss');
+var endTime = moment('22:00:00', 'HH:mm:ss');
+var goalAmount = 200;
+
+var goalData = [];
+var currentAmount = 0;
+var totalHours = moment.duration(endTime.diff(startTime)).asHours();
+var thisTime = moment('00:00:00', 'HH:mm:ss');
+for (i = 0; i <= 24; i++) {
+  if (thisTime.isAfter(startTime) && (thisTime.isBefore(endTime) || thisTime.isSame(endTime))) {
+    currentAmount += goalAmount / totalHours;
+  }
+  goalData.push({
+    x: thisTime.format(),
+    y: currentAmount
+  });
+  thisTime.add(1, 'hour');
+}
+
 var ctx = document.getElementById('dailyProteinChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['12:00AM', '1:00AM', '2:00AM', '3:00AM', '4:00AM', '5:00AM', '6:00AM',
-      '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM',
-      '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM',
-      '7:00PM', '8:00PM', '9:00PM', '10:00PM', '11:00PM', '12:00PM'
-    ],
     datasets: [{
-      data: [0, 0, 0, 0, 0, 0, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-             110, 120, 130, 140, 150, 160, 170, 180],
-      label: "Goal"
-    }, {
-      data: [0, 0, 0, 0, 0, 0, 25, 25, 25, 60, 60, 70, 70, 100, 100, 100,
-             100, 100, 100, 100, 100, 100, 100, 100, 100],
-      label: "Achieved"
+      data: goalData,
+      lineTension: 0,
+      fill: false,
+      label: "goal"
     }]
   },
   options: {
-    annotation: {
-      annotations: [{
-        type: "line",
-        mode: "vertical",
-        scaleID: "x-axis-0",
-        value: "7:00AM",
-        borderColor: "red",
-        label: {
-          content: "TODAY",
-          enabled: true,
-          position: "top"
-        }
+    scales: {
+      xAxes: [{
+          type: 'time'
       }]
+    },
+    annotation: {
+      annotations: [
+        {
+          type: "line",
+          mode: "vertical",
+          scaleID: "x-axis-0",
+          value: moment().format(),
+          borderColor: "red",
+          label: "now"
+        }
+      ]
     }
   }
 });
+
+window.setInterval(function() {
+  myChart.options.annotation.annotations[0].value = moment().format();
+  myChart.update();
+}, 10000);
