2847bd4f726fdad557d344e02b73c2713844d69e
Bryce Anglin <bryce.anglin@gmail.com>
2019-08-25 19:36:25 -0700

    temporarily removed push subscription

--- macros/js/main.js
+++ macros/js/main.js
@@ -24,23 +24,6 @@ self.addEventListener('fetch', function(event) {
   console.log('Fetch!', event.request);
 });
 
-navigator.serviceWorker.ready.then(function(registration) {
-  if (!registration.pushManager) {
-    alert('No push notifications support.');
-    return false;
-  }
-  //To subscribe `push notification` from push manager
-  registration.pushManager.subscribe({
-  userVisibleOnly: true //Always show notification when received
-  })
-  .then(function (subscription) {
-  console.log('Subscribed.');
-  })
-  .catch(function (error) {
-  console.log('Subscription error: ', error);
-  });
-})
-
 window.addEventListener('beforeinstallprompt', e => {
   console.log('beforeinstallprompt Event fired');
   e.preventDefault();
