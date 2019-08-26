507dae545218ad5061872bf4e7602ecc2c94712d
Bryce Anglin <bryce.anglin@gmail.com>
2019-08-25 19:06:21 -0700

    added service worker and manifest file

--- macros/js/main.js
+++ macros/js/main.js
@@ -1,3 +1,47 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+  // Registration was successful
+  console.log('Registered!');
+  }, function(err) {
+  // registration failed :(
+  console.log('ServiceWorker registration failed: ', err);
+  }).catch(function(err) {
+  console.log(err);
+  });
+  });
+  } else {
+  console.log('service worker is not supported');
+  }
+  </script>
+// service-worker.js
+self.addEventListener('install', function() {
+  console.log('Install!');
+});
+self.addEventListener("activate", event => {
+  console.log('Activate!');
+});
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch!', event.request);
+});
+
+navigator.serviceWorker.ready.then(function(registration) {
+  if (!registration.pushManager) {
+    alert('No push notifications support.');
+    return false;
+  }
+  //To subscribe `push notification` from push manager
+  registration.pushManager.subscribe({
+  userVisibleOnly: true //Always show notification when received
+  })
+  .then(function (subscription) {
+  console.log('Subscribed.');
+  })
+  .catch(function (error) {
+  console.log('Subscription error: ', error);
+  });
+})
+
 var startTime = moment('07:00:00', 'HH:mm:ss');
 var endTime = moment('22:00:00', 'HH:mm:ss');
 var goalAmount = 200;
