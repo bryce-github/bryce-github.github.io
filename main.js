4191510dc54fbd53fb2a1bcd836a9e6b31887ceb
bryce93 <bryceanglin@gmail.com>
2020-07-03 07:37:40 -0700

    simplified navigation logic

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-let navElement, activeNavElement = null;
+// handle navigation
+
+let navElement, activeNavElement, activeMainElement, activeElementName;
 const allNavElements = document.querySelectorAll('nav p');
+
+function setActiveNav( navElement ) {
+  if ( activeNavElement ) {
+    activeNavElement.classList.remove('active');
+    activeMainElement.classList.remove('active');
+  }
+  activeNavElement = navElement;
+  activeElementName = activeNavElement.innerText.toLowerCase();
+  activeMainElement = document.querySelector('#'+activeElementName)
+  activeNavElement.classList.add('active');
+  activeMainElement.classList.add('active');
+}
+
 for ( i = 0; i < allNavElements.length; i++ ) {
   navElement = allNavElements[i];
-  navElement.addEventListener('click', (e) => {
-    if ( activeNavElement ) {
-      activeNavElement.classList.remove('active');
-    }
-    activeNavElement = e.target;
-    activeNavElement.classList.add('active');
-  });
+  if ( navElement.classList.contains( 'active' ) ) {
+    setActiveNav( navElement );
+  }
+  navElement.addEventListener( 'click', (e) => {
+    setActiveNav( e.target );
+  })
 }
