abbcd4e4ad3b91d2189ad0d679487f85a8e906ae
Bryce Anglin <bryce.anglin@gmail.com>
2020-07-28 23:54:51 -0700

    updated preflight project to latest

--- projects/preflight/service-worker.js
+++ projects/preflight/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/preflight/precache-manifest.8c82c2e1cf3cdbaf49623f30d17967a5.js"
+  "/projects/preflight/precache-manifest.eafd5a4ccaea2b4193aa17fb29174f03.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "preflight"});
