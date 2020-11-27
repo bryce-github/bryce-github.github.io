95433914a06da2aa4d68ba384212fa21706a09ea
Bryce Anglin <bryce.anglin@gmail.com>
2020-11-27 13:48:54 -0800

    lots of changes to the core resume page

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-// handle navigation
+////////////////
+// NAVIGATION //
+////////////////
 
-let navElement, activeNavElement, activeMainElement, activeElementName;
-const allNavElements = document.querySelectorAll('nav p');
+const background = document.querySelector('.background');
+const backgroundCover = document.querySelector('.background-cover');
 
-function setActiveNav( navElement ) {
-  if ( activeNavElement ) {
+let previousDirection, activeNavIndex = 3;
+let activeNavElement = document.querySelector('nav > div > a.active');
+const navElements = document.querySelectorAll('nav > div > a');
+const navUnderline = document.querySelector('nav > div > hr');
+
+let previousMainElement;
+let activeMainElement = document.querySelector('main > div.home');
+const mainElements = document.querySelectorAll('main > div');
+
+for ( let i = 0; i < navElements.length; i++ ) {
+  let navEl = navElements[i];
+  navEl.addEventListener('click', function(e) {
+    
+    if (e.target === activeNavElement) {
+      return;
+    }
+
+    // TRANSITION THE NAV BAR
     activeNavElement.classList.remove('active');
-    activeMainElement.classList.remove('active');
-  }
-  activeNavElement = navElement;
-  activeElementName = activeNavElement.innerText.toLowerCase();
-  activeMainElement = document.querySelector('#'+activeElementName)
-  activeNavElement.classList.add('active');
-  activeMainElement.classList.add('active');
-}
+    activeNavElement = e.target;
+    activeNavElement.classList.add('active');
+    navUnderline.style.width = activeNavElement.clientWidth + 'px';
+    navUnderline.style.left = activeNavElement.offsetLeft + 'px';
 
-for ( let i = 0; i < allNavElements.length; i++ ) {
-  navElement = allNavElements[i];
-  if ( navElement.classList.contains( 'active' ) ) {
-    setActiveNav( navElement );
-  }
-  navElement.addEventListener( 'click', (e) => {
-    setActiveNav( e.target );
-  })
+    // TRANSITION THE BODY
+    let direction = i < activeNavIndex ? 'left' : 'right';
+    let text = activeNavElement.innerText.toLowerCase();
+
+    // TRANSITION THE OLD ACTIVE MAIN ELEMENT
+    activeMainElement.classList.add('slideout-from-' + direction);
+    activeMainElement.classList.add('hidden');
+    activeMainElement.classList.remove('slidein-from-right');
+    activeMainElement.classList.remove('slidein-from-left');
+    if (previousMainElement) {
+      previousMainElement.classList.remove('slideout-from-right');
+      previousMainElement.classList.remove('slideout-from-left');
+    }
+    previousMainElement = activeMainElement;
+
+    if (text !== 'home') {
+      backgroundCover.classList.add('show')
+    } else {
+      backgroundCover.classList.remove('show')
+    }
+
+    // TRANSITION THE NEW ACTIVE MAIN ELEMENT
+    activeMainElement = document.querySelector('main > div.' + text);
+    activeMainElement.classList.add('slidein-from-' + direction);
+    activeMainElement.classList.remove('hidden');
+    previousDirection = direction;
+    activeNavIndex = i;
+
+  });
 }
